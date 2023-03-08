1bf4c24345b9dfd405538a0142a1eed5ee719d77
beef-erikson <beef.erikson.studios@gmail.com>
2023-03-08 16:10:53 -0500

    Directories set up for Blog Posts, Categories, and Tags. Interface still not fully implemented.

--- Data/BlogApiJsonDirectAccess.cs
+++ Data/BlogApiJsonDirectAccess.cs
@@ -6,5 +6,31 @@ using Data.Models.Interfaces;
 namespace Data;
 public class BlogApiJsonDirectAccess : IBlogApi
 {
-    
+    private BlogApiJsonDirectAccessSetting _settings;
+
+    public BlogApiJsonDirectAccess(IOptions<BlogApiJsonDirectAccessSetting> option)
+    {
+        _settings = option.Value;
+        
+        // Creates directories if not present
+        if (!Directory.Exists((_settings.DataPath)))
+        {
+            Directory.CreateDirectory(_settings.DataPath);
+        }
+
+        if (!Directory.Exists($@"{_settings.DataPath}/{_settings.BlogPostsFolder}"))
+        {
+            Directory.CreateDirectory($@"{_settings.DataPath}/{_settings.BlogPostsFolder}");
+        }
+
+        if (!Directory.Exists($@"{_settings.DataPath}/{_settings.CategoriesFolder}"))
+        {
+            Directory.CreateDirectory($@"{_settings.DataPath}/{_settings.CategoriesFolder}");
+        }
+
+        if (!Directory.Exists($@"{_settings.DataPath}/{_settings.TagsFolder}"))
+        {
+            Directory.CreateDirectory($@"{_settings.DataPath}/{_settings.TagsFolder}");
+        }
+    }
 }
